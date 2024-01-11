83c6a67761043e872bb7da3569b11fedd80e3773
zouker <goncalojbsousa@gmail.com>
2024-01-11 22:08:52 +0000

    teste commit

--- app/src/main/java/com/example/trabalho_pmeu/LoginPage.kt
+++ app/src/main/java/com/example/trabalho_pmeu/LoginPage.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
