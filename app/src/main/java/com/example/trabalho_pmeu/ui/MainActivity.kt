067ca1dbb3592eefb135ed6cebc0b6d9b7e6f27d
J4yP1 <129171739+J4yP1@users.noreply.github.com>
2024-01-12 11:09:43 +0000

    backend innit

--- app/src/main/java/com/example/trabalho_pmeu/ui/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_pmeu/ui/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.trabalho_pmeu.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trabalho_pmeu.R
+import androidx.navigation.fragment.NavHostFragment
+import com.example.trabalho_pmeu.databinding.ActivityMainBinding
+//import com.example.trabalho_pmeu.helper.initToolbar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
--- app/src/main/java/com/example/trabalho_pmeu/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/trabalho_pmeu/ui/auth/LoginFragment.kt
@@ -5,53 +5,97 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.trabalho_pmeu.ARG_PARAM1
-import com.example.trabalho_pmeu.ARG_PARAM2
 import com.example.trabalho_pmeu.R
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.example.trabalho_pmeu.databinding.FragmentLoginPBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+/*import com.example.trabalho_pmeu.databinding.FragmentLoginBinding
+import com.example.trabalho_pmeu.helper.BaseFragment
+import com.example.trabalho_pmeu.helper.FirebaseHelper
+import com.example.trabalho_pmeu.helper.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase*/
+
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLoginPBinding? = null
+    private val binding get() = _binding!!
+
+    //private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_p, container, false)
+    ): View {
+        _binding = FragmentLoginPBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginP.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //auth = Firebase.auth
+
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnLogin.setOnClickListener { validateData() }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginP_to_registerP)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginP_to_recoverFragment)
+        }
+    }
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+
+               // hideKeyboard()
+
+                //binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
+            } else {
+                /*showBottomSheet(
+                    message = R.string.text_password_empty_login_fragment*/
+                )
+            }
+        } else {
+            /*showBottomSheet(
+                message = R.string.text_email_empty_login_fragment*/
+            )
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginP_to_homeP)
+                } else {
+                    /*showBottomSheet(
+                        message = FirebaseHelper.validError(task.exception?.message ?: "")
+                    )*/
+                    //binding.progressBar.isVisible = false
                 }
             }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
