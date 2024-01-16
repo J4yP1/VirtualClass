1bdfd84c92f71cec274b8515127199b869905686
zouker <goncalojbsousa@gmail.com>
2024-01-16 23:05:42 +0000

    created task model and task adapter

--- app/src/main/java/com/example/trabalho_pmeu/ui/TodoFragment.kt
+++ app/src/main/java/com/example/trabalho_pmeu/ui/TodoFragment.kt
@@ -5,15 +5,167 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.trabalho_pmeu.R
+import com.example.trabalho_pmeu.databinding.FragmentTodoBinding
 
 class TodoFragment : Fragment() {
+
+    private var _binding: FragmentTodoBinding? = null
+    private val binding get() = _binding!!
+
+    //private lateinit var taskAdapter: TaskAdapter
+
+    //private val taskList = mutableListOf<Task>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+    ): View {
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClicks()
+
+        //getTasks()
+    }
+
+    private fun initClicks() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeP_to_formTaskFragment)
+        }
+
+    }
+
+    /*private fun getTasks() {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getIdUser() ?: "")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+
+                        taskList.clear()
+                        for (snap in snapshot.children) {
+                            val task = snap.getValue(Task::class.java) as Task
+
+                            if (task.status == 0) taskList.add(task)
+                        }
+
+                        taskList.reverse()
+                        initAdapter()
+                    }
+
+                    tasksEmpty()
+
+                    binding.progressBar.isVisible = false
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(requireContext(), "Erro", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }*/
+
+    /*private fun tasksEmpty() {
+        binding.textInfo.text = if (taskList.isEmpty()) {
+            getText(R.string.text_task_list_empty_todo_fragment)
+        } else {
+            ""
+        }
+    }*/
+
+   /* private fun initAdapter() {
+        binding.rvTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTask.setHasFixedSize(true)
+        taskAdapter = TaskAdapter(requireContext(), taskList) { task, select ->
+            optionSelect(task, select)
+        }
+        binding.rvTask.adapter = taskAdapter
+    }*/
+
+   /* private fun optionSelect(task: Task, select: Int) {
+        when (select) {
+            TaskAdapter.SELECT_REMOVE -> {
+                deleteTask(task)
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                val action = HomeFragmentDirections
+                    .actionHomeFragmentToFormTaskFragment(task)
+                findNavController().navigate(action)
+            }
+            TaskAdapter.SELECT_NEXT -> {
+                task.status = 1
+                updateTask(task)
+            }
+        }
+    }*/
+
+   /* private fun updateTask(task: Task) {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getIdUser() ?: "")
+            .child(task.id)
+            .setValue(task)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.text_task_update_sucess,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    showBottomSheet(message = R.string.error_generic)
+                }
+            }.addOnFailureListener {
+                binding.progressBar.isVisible = false
+                showBottomSheet(message = R.string.error_generic)
+            }
+    }
+
+    private fun deleteTask(task: Task) {
+        showBottomSheet(
+            titleButton = R.string.text_button_confirm,
+            message = R.string.text_message_delete_task_todo_fragment,
+            onClick = {
+                FirebaseHelper
+                    .getDatabase()
+                    .child("task")
+                    .child(FirebaseHelper.getIdUser() ?: "")
+                    .child(task.id)
+                    .removeValue()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                requireContext(),
+                                R.string.text_task_update_sucess,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            showBottomSheet(message = R.string.error_generic)
+                        }
+                    }.addOnFailureListener {
+                        binding.progressBar.isVisible = false
+                        showBottomSheet(message = R.string.error_generic)
+                    }
+
+                taskList.remove(task)
+                taskAdapter.notifyDataSetChanged()
+
+                Toast.makeText(requireContext(), R.string.text_task_delete_sucess, Toast.LENGTH_SHORT).show()
+            }
+        )
+    }*/
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
